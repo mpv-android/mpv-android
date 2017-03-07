485eecfc536c1faac56da40a6c9cfb7bd7d89ead
sfan5 <sfan5@live.de>
2017-03-07 10:14:40 +0100

    x86_64 support

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,6 +3,9 @@ LOCAL_PATH:= $(call my-dir)
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 PREFIX = $(PREFIX64)
 endif
+ifeq ($(TARGET_ARCH_ABI),x86_64)
+PREFIX = $(PREFIX_X64)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libswresample
--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,6 +2,9 @@ APP_ABI := armeabi-v7a
 ifneq ($(PREFIX64),)
 APP_ABI += arm64-v8a
 endif
+ifneq ($(PREFIX_X64),)
+APP_ABI += x86_64
+endif
 
 APP_PLATFORM := android-21
 APP_STL := c++_shared
