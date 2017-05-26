56e90916a4371402dab52140afb7807d3a3bef6e
sfan5 <sfan5@live.de>
2017-05-26 16:22:58 +0200

    it kinda works

--- app/src/main/java/is/xyz/mpv/MainActivity.kt
+++ app/src/main/java/is/xyz/mpv/MainActivity.kt
@@ -1,14 +1,18 @@
 package `is`.xyz.mpv
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment.getExternalStorageDirectory
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.EditText
 
 import com.nononsenseapps.filepicker.AbstractFilePickerFragment
 
@@ -38,7 +42,27 @@ class MainActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFilePicke
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_open_url) {
+            // https://stackoverflow.com/questions/10903754/#answer-10904665
+            var builder = AlertDialog.Builder(this)
+            builder.setTitle("Open URL")
+
+            var input = EditText(this)
+            input.setInputType(InputType.TYPE_CLASS_TEXT)
+            builder.setView(input)
+
+            builder.setPositiveButton("OK", object: DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface, which: Int): Unit {
+                    playFile(input.getText().toString())
+                }
+            })
+            builder.setNegativeButton("Cancel", object: DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface, which: Int): Unit {
+                    dialog.cancel()
+                }
+            })
+            builder.show()
+        } else if (id == R.id.action_settings) {
             val i = Intent(this, SettingsActivity::class.java)
             startActivity(i)
             return true
