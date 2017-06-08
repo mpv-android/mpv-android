56158233e1a42fde06e00a3e3e74160e410f9aa9
sfan5 <sfan5@live.de>
2017-06-08 23:49:09 +0200

    MPVView: Enable MPEG-2 video hwdec

--- app/src/main/java/is/xyz/mpv/MPVView.kt
+++ app/src/main/java/is/xyz/mpv/MPVView.kt
@@ -87,7 +87,7 @@ internal class MPVView(context: Context, attrs: AttributeSet) : GLSurfaceView(co
 
         MPVLib.setOptionString("vo", "opengl-cb")
         MPVLib.setOptionString("hwdec", hwdec)
-        MPVLib.setOptionString("hwdec-codecs", "h264,hevc,mpeg4,vp8,vp9")
+        MPVLib.setOptionString("hwdec-codecs", "h264,hevc,mpeg4,mpeg2video,vp8,vp9")
         MPVLib.setOptionString("ao", "opensles")
         MPVLib.setOptionString("tls-verify", "yes")
         MPVLib.setOptionString("tls-ca-file", "${this.context.filesDir.path}/cacert.pem")
