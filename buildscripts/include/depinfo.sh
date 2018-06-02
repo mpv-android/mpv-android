5127e86fdd812048b73af7bbb1599afa1ec7a8a1
sfan5 <sfan5@live.de>
2018-06-02 13:22:03 +0200

    shaderc build script

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -27,7 +27,8 @@ dep_fribidi=()
 dep_harfbuzz=()
 dep_libass=(freetype2 fribidi harfbuzz)
 dep_lua=()
-dep_mpv=(ffmpeg libass lua)
+dep_shaderc=()
+dep_mpv=(ffmpeg libass lua shaderc)
 dep_mpv_android=(mpv)
 
 
---
+++ buildscripts/scripts/shaderc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+. ../../include/path.sh
+
+if [ "$1" == "build" ]; then
+	true
+elif [ "$1" == "clean" ]; then
+	rm -rf local include libs
+	exit 0
+else
+	exit 255
+fi
+
+builddir=$PWD
+application_mk=$PWD/../../../app/src/main/jni/Application.mk # APP_{PLATFORM,STL} are imported from here
+
+abi=armeabi-v7a
+[[ "$ndk_triple" == "aarch64"* ]] && abi=arm64-v8a
+[[ "$ndk_triple" == "x86_64"* ]] && abi=x86_64
+[[ "$ndk_triple" == "i686"* ]] && abi=x86
+
+# build using the NDK's scripts, but keep object files in our build dir
+cd "$(dirname "$(which ndk-build)")/sources/third_party/shaderc"
+ndk-build -j$cores \
+	NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=Android.mk \
+	NDK_APPLICATION_MK="$application_mk" APP_ABI=$abi \
+	NDK_APP_OUT="$builddir" NDK_APP_LIBS_OUT="$builddir/libs" \
+	libshaderc_combined
+
+cd "$builddir"
+cp -r include/* "$prefix_dir/include"
+cp libs/*/$abi/libshaderc.a "$prefix_dir/lib/libshaderc_combined.a"
+
+# create a pkgconfig file
+mkdir -p "$prefix_dir"/lib/pkgconfig
+cat >"$prefix_dir"/lib/pkgconfig/shaderc_combined.pc <<"END"
+Name: shaderc_combined
+Description:
+Version: 2021.0-unknown
+Libs: -L/usr/lib -lshaderc_combined -lstdc++
+Cflags: -I/usr/include
+END
