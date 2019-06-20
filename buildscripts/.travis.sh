4cc87cbc99c2be4682c55890b0102bf4d755cce2
sfan5 <sfan5@live.de>
2019-06-20 22:43:49 +0200

    travis: stop uploading APK

--- buildscripts/.travis.sh
+++ buildscripts/.travis.sh
@@ -62,7 +62,4 @@ echo "==> Building mpv"
 echo "==> Building mpv-android"
 ./buildall.sh --no-deps
 
-echo "==> Uploading the .apk"
-curl -F'file=@../app/build/outputs/apk/debug/app-debug.apk' http://0x0.st
-
 exit 0
