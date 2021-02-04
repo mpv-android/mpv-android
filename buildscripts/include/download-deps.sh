6cc2d0e6fea5d630a11eaa99ee84a4716793bb1a
sfan5 <sfan5@live.de>
2021-02-04 22:17:30 +0100

    buildscripts: make download script skip libraries already downloaded

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -8,48 +8,49 @@
 mkdir -p deps && cd deps
 
 # mbedtls
-mkdir mbedtls
-cd mbedtls
-$WGET https://github.com/ARMmbed/mbedtls/archive/mbedtls-$v_mbedtls.tar.gz -O - | \
-	tar -xz --strip-components=1
-cd ..
+if [ ! -d mbedtls ]; then
+	mkdir mbedtls
+	$WGET https://github.com/ARMmbed/mbedtls/archive/mbedtls-$v_mbedtls.tar.gz -O - | \
+		tar -xz -C mbedtls --strip-components=1
+fi
 
 # dav1d
-git clone https://code.videolan.org/videolan/dav1d.git
+[ ! -d dav1d ] && git clone https://code.videolan.org/videolan/dav1d.git
 
 # ffmpeg
-git clone https://github.com/FFmpeg/FFmpeg ffmpeg
-[ $TRAVIS -eq 1 ] && ( cd ffmpeg; git checkout $v_travis_ffmpeg )
+if [ ! -d ffmpeg ]; then
+	git clone https://github.com/FFmpeg/FFmpeg ffmpeg
+	[ $TRAVIS -eq 1 ] && ( cd ffmpeg; git checkout $v_travis_ffmpeg )
+fi
 
 # freetype2
-git clone git://git.sv.nongnu.org/freetype/freetype2.git -b VER-$v_freetype
+[ ! -d freetype2 ] && git clone git://git.sv.nongnu.org/freetype/freetype2.git -b VER-$v_freetype
 
 # fribidi
-mkdir fribidi
-cd fribidi
-$WGET https://github.com/fribidi/fribidi/releases/download/v$v_fribidi/fribidi-$v_fribidi.tar.xz -O - | \
-	tar -xJ --strip-components=1
-cd ..
+if [ ! -d fribidi ]; then
+	mkdir fribidi
+	$WGET https://github.com/fribidi/fribidi/releases/download/v$v_fribidi/fribidi-$v_fribidi.tar.xz -O - | \
+		tar -xJ -C fribidi --strip-components=1
+fi
 
 # harfbuzz
-mkdir harfbuzz
-cd harfbuzz
-$WGET https://github.com/harfbuzz/harfbuzz/releases/download/$v_harfbuzz/harfbuzz-$v_harfbuzz.tar.xz -O - | \
-	tar -xJ --strip-components=1
-cd ..
+if [ ! -d harfbuzz ]; then
+	mkdir harfbuzz
+	$WGET https://github.com/harfbuzz/harfbuzz/releases/download/$v_harfbuzz/harfbuzz-$v_harfbuzz.tar.xz -O - | \
+		tar -xJ -C harfbuzz --strip-components=1
+fi
 
 # libass
-git clone https://github.com/libass/libass
+[ ! -d libass ] && git clone https://github.com/libass/libass
 
 # lua
-mkdir lua
-cd lua
-$WGET http://www.lua.org/ftp/lua-$v_lua.tar.gz -O - | \
-	tar -xz --strip-components=1
-cd ..
+if [ ! -d lua ]; then
+	mkdir lua
+	$WGET http://www.lua.org/ftp/lua-$v_lua.tar.gz -O - | \
+		tar -xz -C lua --strip-components=1
+fi
 
-# mpv (travis downloads a tar.gz snapshot instead)
-[ $TRAVIS -eq 0 ] && \
-	git clone https://github.com/mpv-player/mpv
+# mpv
+[ ! -d mpv ] && git clone https://github.com/mpv-player/mpv
 
 cd ..
