00765f2d01cc5e827987d028002905d242db5fbf
sfan5 <sfan5@live.de>
2021-03-02 15:27:09 +0100

    buildscripts: switch fribidi build to meson
    closes #376

--- buildscripts/scripts/fribidi.sh
+++ buildscripts/scripts/fribidi.sh
@@ -2,23 +2,22 @@
 
 . ../../include/path.sh
 
+build=_build$ndk_suffix
+
 if [ "$1" == "build" ]; then
 	true
 elif [ "$1" == "clean" ]; then
-	rm -rf _build$ndk_suffix
+	rm -rf $build
 	exit 0
 else
 	exit 255
 fi
 
-mkdir -p _build$ndk_suffix
-cd _build$ndk_suffix
+unset CC CXX # meson wants these unset
 
-PKG_CONFIG=/bin/false \
-../configure \
-	--host=$ndk_triple \
-	--enable-static --disable-shared \
-	--disable-docs
+meson $build \
+	--buildtype release --cross-file "$prefix_dir"/crossfile.txt \
+	--default-library static -D{tests,docs}=false
 
-make -j$cores
-make DESTDIR="$prefix_dir" install
+ninja -C $build -j$cores
+DESTDIR="$prefix_dir" ninja -C $build install
