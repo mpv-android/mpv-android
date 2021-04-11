810e28db1a8579bff9ac6b3e5d31b8a830dcfe9d
sfan5 <sfan5@live.de>
2021-04-11 17:06:16 +0200

    buildscripts: update NDK and dependencies

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -3,14 +3,14 @@
 ## Dependency versions
 
 v_sdk=6609375_latest
-v_ndk=r22
+v_ndk=r22b
 v_sdk_build_tools=29.0.2
 
 v_lua=5.2.4
-v_harfbuzz=2.7.4
+v_harfbuzz=2.8.0
 v_fribidi=1.0.10
 v_freetype=2-10-4
-v_mbedtls=2.25.0
+v_mbedtls=2.26.0
 
 
 ## Dependency tree
--- buildscripts/include/path.sh
+++ buildscripts/include/path.sh
@@ -23,6 +23,6 @@ if [ -n "$ndk_triple" ]; then
 	unset PKG_CONFIG_PATH
 fi
 
-toolchain=$(echo "$DIR/sdk/android-ndk-r22/toolchains/llvm/prebuilt/"*)
-export PATH="$toolchain/bin:$DIR/sdk/android-ndk-r22:$DIR/sdk/bin:$PATH"
+toolchain=$(echo "$DIR/sdk/android-ndk-r22b/toolchains/llvm/prebuilt/"*)
+export PATH="$toolchain/bin:$DIR/sdk/android-ndk-r22b:$DIR/sdk/bin:$PATH"
 export ANDROID_HOME="$DIR/sdk/android-sdk-$os"
