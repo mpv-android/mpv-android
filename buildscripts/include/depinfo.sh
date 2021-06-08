11141182bebf6836c7f7d83245427993fc7e40d3
sfan5 <sfan5@live.de>
2021-06-08 21:10:07 +0200

    buildscripts: update harfbuzz and pinned ffmpeg commit

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -7,7 +7,7 @@ v_ndk=r22b
 v_sdk_build_tools=29.0.2
 
 v_lua=5.2.4
-v_harfbuzz=2.8.0
+v_harfbuzz=2.8.1
 v_fribidi=1.0.10
 v_freetype=2-10-4
 v_mbedtls=2.26.0
@@ -31,7 +31,7 @@ dep_mpv_android=(mpv)
 ## Travis-related
 
 # pinned ffmpeg commit used by travis-ci
-v_travis_ffmpeg=53db591a2e2b1d8075aef96ef32a7c4d4a64111d
+v_travis_ffmpeg=812f39ee85ac27a7e11a374150e60a4614f060b1
 
 # filename used to uniquely identify a build prefix
 travis_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-harfbuzz-${v_harfbuzz}-fribidi-${v_fribidi}-freetype-${v_freetype}-mbedtls-${v_mbedtls}-ffmpeg-${v_travis_ffmpeg}.tgz"
