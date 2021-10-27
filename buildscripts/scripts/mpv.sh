0d86c6bdfe708538271670f8fafb22a781713f3d
sfan5 <sfan5@live.de>
2021-10-28 00:39:39 +0200

    buildscripts: remove workaround from mpv.sh

--- buildscripts/scripts/mpv.sh
+++ buildscripts/scripts/mpv.sh
@@ -13,11 +13,10 @@ fi
 
 [ -f waf ] || ./bootstrap.py
 
-# ffmpeg-strict-abi: to be removed once https://github.com/mpv-player/mpv/issues/9159 is solved
 PKG_CONFIG="pkg-config --static" \
 ./waf configure \
 	--disable-iconv --lua=52 \
-	--enable-libmpv-shared --enable-ffmpeg-strict-abi \
+	--enable-libmpv-shared \
 	--disable-manpage-build \
 	-o "`pwd`/_build$ndk_suffix"
 
