72838152a5411cd933c01ff7651502a0d4aa90da
Aravind Ceyardass <aravind.pub@gmail.com>
2021-11-09 16:22:02 -0500

    Add libmysofa version number & update ffmpeg dependency

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -11,14 +11,15 @@ v_harfbuzz=3.0.0
 v_fribidi=1.0.11
 v_freetype=2-11-0
 v_mbedtls=2.27.0
-
+v_libmysofa=1.2.1
 
 ## Dependency tree
 # I would've used a dict but putting arrays in a dict is not a thing
 
 dep_mbedtls=()
 dep_dav1d=()
-dep_ffmpeg=(mbedtls dav1d)
+dep_libmysofa=()
+dep_ffmpeg=(mbedtls dav1d libmysofa)
 dep_freetype2=()
 dep_fribidi=()
 dep_harfbuzz=()
@@ -34,4 +35,4 @@ dep_mpv_android=(mpv)
 v_travis_ffmpeg=75001ae8440d819d23443709091fca4c39e395a1
 
 # filename used to uniquely identify a build prefix
-travis_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-harfbuzz-${v_harfbuzz}-fribidi-${v_fribidi}-freetype-${v_freetype}-mbedtls-${v_mbedtls}-ffmpeg-${v_travis_ffmpeg}.tgz"
+travis_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-harfbuzz-${v_harfbuzz}-fribidi-${v_fribidi}-freetype-${v_freetype}-mbedtls-${v_mbedtls}-libmysofa-${v_libmysofa}-ffmpeg-${v_travis_ffmpeg}.tgz"
