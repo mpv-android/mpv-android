bbf30680d206db8686a82a55946f3fad46499041
sfan5 <sfan5@live.de>
2022-03-05 20:45:51 +0100

    buildscripts: bump deps & pinned ffmpeg commit

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -7,7 +7,7 @@ v_ndk=r23b
 v_sdk_build_tools=30.0.2
 
 v_lua=5.2.4
-v_harfbuzz=3.1.2
+v_harfbuzz=4.0.0
 v_fribidi=1.0.11
 v_freetype=2-11-1
 v_mbedtls=2.28.0
@@ -31,7 +31,7 @@ dep_mpv_android=(mpv)
 ## Travis-related
 
 # pinned ffmpeg commit used by travis-ci
-v_travis_ffmpeg=75001ae8440d819d23443709091fca4c39e395a1
+v_travis_ffmpeg=954279564a0ae38e72ce828a90c7d57f9b90a29e
 
 # filename used to uniquely identify a build prefix
 travis_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-harfbuzz-${v_harfbuzz}-fribidi-${v_fribidi}-freetype-${v_freetype}-mbedtls-${v_mbedtls}-ffmpeg-${v_travis_ffmpeg}.tgz"
