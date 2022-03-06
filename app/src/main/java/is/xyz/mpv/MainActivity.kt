03293a7db2d373da4b44bb12a95b6c6ddf967d2a
tarsin <43900799+asuka-mio@users.noreply.github.com>
2022-03-06 17:11:55 +0800

    clean: remove mainpage unused code

--- app/src/main/java/is/xyz/mpv/MainActivity.kt
+++ app/src/main/java/is/xyz/mpv/MainActivity.kt
@@ -5,17 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
-import android.app.UiModeManager
-import android.content.res.Configuration
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.core.content.ContextCompat
 
 import `is`.xyz.mpv.databinding.ActivityMainBinding
 import android.view.MenuItem
 import com.xayah.materialyoufileexplorer.MaterialYouFileExplorer
 
 class MainActivity : AppCompatActivity() {
+    lateinit var materialYouFileExplorer:MaterialYouFileExplorer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,39 +20,17 @@ class MainActivity : AppCompatActivity() {
 
         materialYouFileExplorer = MaterialYouFileExplorer()
 
-        if (PackageManager.PERMISSION_GRANTED ==
-            ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            materialYouFileExplorer.initialize(this)
-            materialYouFileExplorer.toExplorer(this, true) { path, _ -> playFile(path) }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // restart activity to init file picker correctly
-            finish()
-            startActivity(intent)
-        }
+        materialYouFileExplorer.initialize(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val uiModeManager = getSystemService(UI_MODE_SERVICE) as UiModeManager
-        if (uiModeManager.currentModeType != Configuration.UI_MODE_TYPE_TELEVISION)
-            menuInflater.inflate(R.menu.menu_main, menu)
-        else
-            menu.add(Menu.NONE, Menu.NONE, Menu.NONE, "...") // dummy menu item to indicate presence
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
 
-        when (id) {
+        when (item.itemId) {
             R.id.action_settings -> {
                 val i = Intent(this, SettingsActivity::class.java)
                 startActivity(i)
@@ -63,7 +38,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             R.id.action_external_storage -> {
-                materialYouFileExplorer.toExplorer(this, true) { path, _ -> playFile(path) }
+                selectFileToPlay()
                 return true
             }
         }
@@ -76,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         startActivity(i)
     }
 
-    companion object {
-        lateinit var materialYouFileExplorer:MaterialYouFileExplorer
+    private fun selectFileToPlay() {
+        materialYouFileExplorer.toExplorer(this, true) { path, _ -> playFile(path) }
     }
 }
