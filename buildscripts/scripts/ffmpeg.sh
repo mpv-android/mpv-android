955ba5908f7fc116244a3b9863d22689726a4e2b
TyraVex <thomas.baruzier@gmail.com>
2022-05-12 11:25:07 +0200

    smaller ffmpeg binaries to reduce app size

--- buildscripts/scripts/ffmpeg.sh
+++ buildscripts/scripts/ffmpeg.sh
@@ -23,12 +23,19 @@ cpuflags=
 [[ "$ndk_triple" == "arm"* ]] && cpuflags="$cpuflags -mfpu=neon -mcpu=cortex-a8"
 
 ../configure \
-	--target-os=android --enable-cross-compile --cross-prefix=$ndk_triple- --cc=$CC \
-	--arch=${ndk_triple%%-*} --cpu=$cpu --enable-{jni,mediacodec,mbedtls,libdav1d} \
+	--target-os=android --cross-prefix=$ndk_triple- --cc=$CC \
+	--arch=${ndk_triple%%-*} --cpu=$cpu --enable-{jni,mediacodec,libdav1d} \
 	--extra-cflags="-I$prefix_dir/include $cpuflags" --extra-ldflags="-L$prefix_dir/lib" \
 	--disable-static --enable-shared --enable-{gpl,version3} \
 	--pkg-config=pkg-config --disable-{stripping,doc,programs} \
-	--disable-{muxers,encoders,devices} --enable-encoder=mjpeg,png
+  --disable-programs --enable-small --disable-runtime-cpudetect --disable-swscale-alpha \
+  --disable-avdevice --enable-swresample --disable-swscale --disable-postproc --disable-w32threads --disable-os2threads --disable-network --disable-dct --disable-dwt --disable-error-resilience --disable-lsp --disable-mdct --disable-rdft --enable-fft --disable-faan --disable-pixelutils \
+  --disable-filters --enable-hwaccels --disable-muxers --disable-parsers --disable-encoders --disable-bsfs --disable-protocols --enable-protocol=file --disable-indevs --disable-outdevs --disable-devices \
+  --disable-alsa --disable-appkit --disable-avfoundation --disable-bzlib --disable-coreimage --disable-iconv --disable-lzma --disable-metal --disable-sndio --disable-schannel --disable-sdl2 --disable-securetransport --disable-vulkan --disable-xlib --disable-zlib \
+  --disable-audiotoolbox --disable-amf --disable-cuda-llvm --disable-cuvid --disable-d3d11va --disable-dxva2 --disable-ffnvcodec --disable-nvdec --disable-nvenc --disable-vaapi --disable-vdpau --disable-v4l2-m2m --disable-videotoolbox --disable-debug
+
+  # for an av1/opus capabilities only
+  # --disable-demuxers --enable-demuxer=ogg,mov --disable-decoders --enable-decoder=mjpegb,movtext,opus,libdav1d \
 
 make -j$cores
 make DESTDIR="$prefix_dir" install
