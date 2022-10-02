ab9c3eb006ad9ace6c49829806ae4576df5c5c3a
sfan5 <sfan5@live.de>
2022-10-02 17:48:20 +0200

    buildscripts: bump NDK and dependencies

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -5,12 +5,12 @@
 # also remember to update path.sh
 
 v_sdk=8512546_latest
-v_ndk=r25
-v_ndk_n=25.0.8775105
+v_ndk=r25b
+v_ndk_n=25.1.8937393
 v_sdk_build_tools=30.0.3
 
 v_lua=5.2.4
-v_harfbuzz=4.4.1
+v_harfbuzz=5.2.0
 v_fribidi=1.0.12
 v_freetype=2-12-1
 v_mbedtls=2.28.1
