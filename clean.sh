529e180d96f4a67950c61374c256cf004bb4a19e
syphyr <syphyr@gmail.com>
2023-05-22 01:03:44 +0200

    Add cleaning script

---
+++ clean.sh
@@ -0,0 +1,32 @@
+git clean -fdx -e buildscripts
+cd buildscripts
+git clean -fdx -e deps -e sdk -e dist.zip
+
+cd deps/dav1d
+git clean -fdx
+git reset --hard
+
+cd ../elf-cleaner
+git clean -fdx
+git reset --hard
+
+cd ../ffmpeg
+git clean -fdx
+git reset --hard
+
+cd ../freetype2
+git clean -fdx
+git submodule foreach --recursive git clean -xfd
+git reset --hard
+git submodule foreach --recursive git reset --hard
+
+cd ../libass
+git clean -fdx
+git reset --hard
+
+cd ../mpv
+git clean -fdx
+git reset --hard
+
+cd ..
+git clean -fdx -e dav1d -e elf-cleaner -e ffmpeg -e freetype2 -e libass -e mpv -e "*.tar.gz" -e "*.tar.xz"
