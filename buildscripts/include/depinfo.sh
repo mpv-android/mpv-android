3ec5f76332eeb2da3f7617f6bd04f8abcfd26ea2
sfan5 <sfan5@live.de>
2023-08-05 21:39:50 +0200

    buildscripts: update dependency libs

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -10,10 +10,10 @@ v_ndk_n=25.2.9519653
 v_sdk_build_tools=30.0.3
 
 v_lua=5.2.4
-v_harfbuzz=7.3.0
-v_fribidi=1.0.12
-v_freetype=2-13-0
-v_mbedtls=2.28.3
+v_harfbuzz=8.1.1
+v_fribidi=1.0.13
+v_freetype=2-13-1
+v_mbedtls=2.28.4
 
 
 ## Dependency tree
