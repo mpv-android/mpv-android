ce8dd871acfe43cca13aaad54d0d52719c04b51e
sfan5 <sfan5@live.de>
2023-10-23 23:43:09 +0200

    enable vulkan

--- buildscripts/scripts/libplacebo.sh
+++ buildscripts/scripts/libplacebo.sh
@@ -15,7 +15,7 @@ fi
 
 unset CC CXX
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-	-Dvulkan=disabled -Ddemos=false
+	-Dvk-proc-addr=enabled -Ddemos=false
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
