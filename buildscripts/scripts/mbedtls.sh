63cdf1eabf25808e8f6992a5392c14840e30446b
sfan5 <sfan5@live.de>
2023-11-26 19:07:50 +0100

    buildscripts: fix mbedtls on x86

--- buildscripts/scripts/mbedtls.sh
+++ buildscripts/scripts/mbedtls.sh
@@ -12,6 +12,11 @@ else
 fi
 
 $0 clean # separate building not supported, always clean
+if [[ "$ndk_triple" == "i686"* ]]; then
+	./scripts/config.py unset MBEDTLS_AESNI_C
+else
+	./scripts/config.py set MBEDTLS_AESNI_C
+fi
 
 make -j$cores no_test
 make DESTDIR="$prefix_dir" install
