mpv-android/mpv-android

commit 22ae4040e6dcb975da357d43b0966913a57d4ecd

MPVActivity: stop showing UI on every key press
Not sure what the original idea behind this way, but it's definitely in the way.
closes #813

app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -645,7 +645,6 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
                 (ev.action == KeyEvent.ACTION_DOWN && interceptKeyDown(ev)) ||
                 player.onKey(ev)
         if (handled) {
-            showControls()
             return true
         }
         return super.dispatchKeyEvent(ev)
@@ -695,6 +694,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
                     if (ev.action == KeyEvent.ACTION_DOWN) { // activate dpad navigation
                         btnSelected = 0
                         updateShowBtnSelected()
+                        showControls()
                     }
                     return true
                 }

commit 40b6d45c092a6bd14871413cd8d7f70714f7d3cf

MPVActivity: fix autohide with dpad navigation
fixes #894
broken by 22ae4040e6dcb975da357d43b0966913a57d4ecd

app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -744,6 +744,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
                 if (ev.action == KeyEvent.ACTION_DOWN) { // deactivate dpad navigation
                     btnSelected = -1
                     updateSelectedDpadButton()
+                    hideControlsDelayed()
                 }
                 return true
             }