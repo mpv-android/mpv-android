mpv-android/mpv-android

commit ded0803a36f20b4de6fefb3cef4ff4fc3858bf30

MPVActivity: re-add rotation logic to aspect ratio calculation
Not sure what I did in c7f2b31e4e5e45bb236d679a13f48a7798a97066
because it seems mpv's video-params/aspect never respected the rotation
metadata. Oh well.

Also switch to video-out-params since that's after video filters (if any).
fixes #890

app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -1535,7 +1535,11 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         if (initial || player.vid == -1)
             return
 
-        val ratio = player.videoAspect?.toFloat() ?: 0f
+        var ratio = player.videoOutAspect?.toFloat() ?: 0f
+        if (ratio != 0f) {
+            if ((player.videoOutRotation ?: 0) % 180 == 90)
+                ratio = 1f / ratio
+        }
         Log.v(TAG, "auto rotation: aspect ratio = $ratio")
 
         if (ratio == 0f || ratio in (1f / ASPECT_RATIO_MIN) .. ASPECT_RATIO_MIN) {
@@ -1564,7 +1568,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         }
 
         val params = with(PictureInPictureParams.Builder()) {
-            val aspect = player.videoAspect ?: 1.0
+            val aspect = player.videoOutAspect ?: 1.0
             setAspectRatio(Rational(aspect.times(10000).toInt(), 10000))
             setActions(listOf(action1))
         }

app/src/main/java/is/xyz/mpv/MPVView.kt
@@ -334,8 +334,11 @@ internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(cont
     val estimatedVfFps: Double?
         get() = MPVLib.getPropertyDouble("estimated-vf-fps")
 
-    val videoAspect: Double?
-        get() = MPVLib.getPropertyDouble("video-params/aspect")
+    val videoOutAspect: Double?
+        get() = MPVLib.getPropertyDouble("video-out-params/aspect")
+
+    val videoOutRotation: Int?
+        get() = MPVLib.getPropertyInt("video-out-params/rotate")
 
     class TrackDelegate(private val name: String) {
         operator fun getValue(thisRef: Any?, property: KProperty<*>): Int {

commit 12bfad77a5026357e1338cce622d3fd579022cfd

MPVView: revert back to using aspect before filters
Otherwise the automatic rotation will work "against" the user
setting --video-rotate.
This reverts to the old way of using the information as returned
from decoding, so won't work for complex filter chains.

fixes: ded0803a36f20b4de6fefb3cef4ff4fc3858bf30
closes #933

app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -1576,7 +1576,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         if (initial || player.vid == -1)
             return
 
-        val ratio = player.getVideoOutAspect()?.toFloat() ?: 0f
+        val ratio = player.getVideoAspect()?.toFloat() ?: 0f
         if (ratio == 0f || ratio in (1f / ASPECT_RATIO_MIN) .. ASPECT_RATIO_MIN) {
             // video is square, let Android do what it wants
             requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
@@ -1603,7 +1603,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         }
 
         val params = with(PictureInPictureParams.Builder()) {
-            val aspect = player.getVideoOutAspect() ?: 0.0
+            val aspect = player.getVideoAspect() ?: 0.0
             setAspectRatio(Rational(aspect.times(10000).toInt(), 10000))
             setActions(listOf(action1))
         }
@@ -1663,7 +1663,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         if (!activityIsForeground) return
         when (property) {
             "track-list" -> player.loadTracks()
-            "video-out-params/aspect", "video-out-params/rotate" -> {
+            "video-params/aspect", "video-params/rotate" -> {
                 updateOrientation()
                 updatePiPParams()
             }

app/src/main/java/is/xyz/mpv/MPVView.kt
@@ -9,7 +9,6 @@ import android.os.Build
 import android.os.Environment
 import android.preference.PreferenceManager
 import android.view.*
-import kotlin.math.abs
 import kotlin.reflect.KProperty
 
 internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(context, attrs), SurfaceHolder.Callback {
@@ -218,8 +217,8 @@ internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(cont
             Property("track-list"),
             // observing double properties is not hooked up in the JNI code, but doing this
             // will restrict updates to when it actually changes
-            Property("video-out-params/aspect", MPV_FORMAT_DOUBLE),
-            Property("video-out-params/rotate", MPV_FORMAT_DOUBLE),
+            Property("video-params/aspect", MPV_FORMAT_DOUBLE),
+            Property("video-params/rotate", MPV_FORMAT_DOUBLE),
             //
             Property("playlist-pos", MPV_FORMAT_INT64),
             Property("playlist-count", MPV_FORMAT_INT64),
@@ -341,14 +340,13 @@ internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(cont
         get() = MPVLib.getPropertyDouble("estimated-vf-fps")
 
     /**
-     * Returns the video aspect ratio after video filters (before VO).
-     * Rotation is taken into account.
+     * Returns the video aspect ratio. Rotation is taken into account.
      */
-    fun getVideoOutAspect(): Double? {
-        return MPVLib.getPropertyDouble("video-out-params/aspect")?.let {
+    fun getVideoAspect(): Double? {
+        return MPVLib.getPropertyDouble("video-params/aspect")?.let {
             if (it < 0.001)
                 return 0.0
-            val rot = MPVLib.getPropertyInt("video-out-params/rotate") ?: 0
+            val rot = MPVLib.getPropertyInt("video-params/rotate") ?: 0
             if (rot % 180 == 90)
                 1.0 / it
             else