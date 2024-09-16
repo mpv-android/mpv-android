ecdd3c6aaaa9b1e4b3011b72b849698de1de62c7
Khaled <spkhalad@gmail.com>
2024-09-16 22:29:06 +0600

    feat: Implement BrowseActivity Layouts in Material3

---
+++ app/src/main/java/is/xyz/ui/BrowseActivity.kt
@@ -0,0 +1,21 @@
+package `is`.xyz.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import `is`.xyz.mpv.R
+
+class BrowseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_browse)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.browse_root)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
