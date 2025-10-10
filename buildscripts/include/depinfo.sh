55069639d1f5b020c4a08f19fa13957b376223ad
sfan5 <sfan5@live.de>
2025-10-10 12:44:38 +0200

    buildscripts: update NDK

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -4,8 +4,8 @@
 # Make sure to keep v_ndk and v_ndk_n in sync, both are listed on the NDK download page
 
 v_sdk=11076708_latest
-v_ndk=r28c
-v_ndk_n=28.2.13676358
+v_ndk=r29
+v_ndk_n=29.0.14206865
 v_sdk_platform=35
 v_sdk_build_tools=35.0.0
 
