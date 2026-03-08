8454d8a18bfcf5ae4d37efe14d44c637dc95faa4
sfan5 <sfan5@live.de>
2026-03-08 12:13:22 +0100

    buildscripts: update libs

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -11,9 +11,9 @@ v_sdk_build_tools=35.0.0
 
 v_lua=5.2.4
 v_unibreak=6.1
-v_harfbuzz=12.3.2
+v_harfbuzz=13.0.1
 v_fribidi=1.0.16
-v_freetype=2.14.1
+v_freetype=2.14.2
 v_mbedtls=3.6.5
 
 
@@ -37,7 +37,7 @@ dep_mpv_android=(mpv)
 ## for CI workflow
 
 # pinned ffmpeg revision
-v_ci_ffmpeg=n8.0
+v_ci_ffmpeg=n8.0.1
 
 # filename used to uniquely identify a build prefix
 ci_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-unibreak-${v_unibreak}-harfbuzz-${v_harfbuzz}-fribidi-${v_fribidi}-freetype-${v_freetype}-mbedtls-${v_mbedtls}-ffmpeg-${v_ci_ffmpeg}.tgz"
