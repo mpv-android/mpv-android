f981b2a12fa45f2897b82285ac48ae09776d5b80
sfan5 <sfan5@live.de>
2026-05-16 21:55:49 +0200

    buildscripts: add curl

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -17,6 +17,7 @@ v_freetype=2.14.3
 v_mbedtls=3.6.5
 v_libxml2=2.15.3
 v_fontconfig=2.17.1
+v_curl=8.20.0
 
 
 ## Dependency tree
@@ -33,7 +34,8 @@ dep_unibreak=()
 dep_libass=(freetype2 fontconfig fribidi harfbuzz unibreak)
 dep_lua=()
 dep_libplacebo=()
-dep_mpv=(ffmpeg libass lua libplacebo)
+dep_curl=(mbedtls)
+dep_mpv=(ffmpeg libass lua libplacebo curl)
 dep_mpv_android=(mpv)
 
 
@@ -43,4 +45,4 @@ dep_mpv_android=(mpv)
 v_ci_ffmpeg=n8.1.1
 
 # filename used to uniquely identify a build prefix
-ci_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-unibreak-${v_unibreak}-harfbuzz-${v_harfbuzz}-fribidi-${v_fribidi}-freetype-${v_freetype}-libxml2-${v_libxml2}-fontconfig-${v_fontconfig}-mbedtls-${v_mbedtls}-ffmpeg-${v_ci_ffmpeg}.tgz"
+ci_tarball="prefix-ndk-${v_ndk}-lua-${v_lua}-unibreak-${v_unibreak}-harfbuzz-${v_harfbuzz}-fribidi-${v_fribidi}-freetype-${v_freetype}-libxml2-${v_libxml2}-fontconfig-${v_fontconfig}-mbedtls-${v_mbedtls}-curl-${v_curl}-ffmpeg-${v_ci_ffmpeg}.tgz"
