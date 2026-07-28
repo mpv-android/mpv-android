9ad9c68593e7789b69b6aaa9800aa34317587a41
SJD Ayy <1sjd@disroot.org>
2026-07-29 03:56:01 +0600

    buildscripts: always download NDK from the web
    Web is Google's preferred way of distributing NDK. Currently
    beta versions cannot be obtained using sdkmanager. Remove its
    use for NDK download.

--- buildscripts/include/download-sdk.sh
+++ buildscripts/include/download-sdk.sh
@@ -65,22 +65,23 @@ echo y | sdkmanager \
 	"platforms;android-${v_sdk_platform}" "build-tools;${v_sdk_build_tools}" \
 	"extras;android;m2repository"
 
-# Android NDK (either standalone or installed by SDK)
+# Android NDK (standalone download)
 if [ -d "android-ndk-${v_ndk}" ]; then
 	echo "Android NDK directory found."
-elif [ -d "android-sdk-$os/ndk/${v_ndk_n}" ]; then
-	echo "Creating NDK symlink to SDK."
-	ln -s "android-sdk-$os/ndk/${v_ndk_n}" "android-ndk-${v_ndk}"
-elif [ -z "${os_ndk}" ]; then
-	echo "Downloading NDK with sdkmanager."
-	echo y | sdkmanager "ndk;${v_ndk_n}"
-	ln -s "android-sdk-$os/ndk/${v_ndk_n}" "android-ndk-${v_ndk}"
 else
 	echo "Downloading NDK."
-	$WGET "http://dl.google.com/android/repository/android-ndk-${v_ndk}-${os_ndk}.zip"
+	$WGET "https://dl.google.com/android/repository/android-ndk-${v_ndk}-${os_ndk}.zip"
 	unzip -q "android-ndk-${v_ndk}-${os_ndk}.zip"
 	rm "android-ndk-${v_ndk}-${os_ndk}.zip"
 fi
+
+# Symlink to create expected directory structure for AGP
+if [ ! -d "android-sdk-$os/ndk/${v_ndk_n}" ]; then
+	echo "Creating NDK symlink inside Android SDK."
+	mkdir -p "android-sdk-$os/ndk"
+	ln -sf "$PWD/android-ndk-${v_ndk}" "android-sdk-$os/ndk/${v_ndk_n}"
+fi
+
 if ! grep -qF "${v_ndk_n}" "android-ndk-${v_ndk}/source.properties"; then
 	echo "Error: NDK exists but is not the correct version (expecting ${v_ndk_n})"
 	exit 255
