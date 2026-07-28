mpv-android/mpv-android

commit 5eb28a427eecd9f7f954a5242b958dc623e152a2

BackgroundPlaybackService: skip unnecessary notification customization attempts

app/src/main/java/is/xyz/mpv/BackgroundPlaybackService.kt
@@ -74,15 +74,19 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
             setOngoing(true)
         }
 
-        thumbnail?.let {
-            builder.setLargeIcon(it)
+        // With an active media session, the media style will override everything
+        // (including the thumbnail) and we can skip doing this.
+        if (mediaToken != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            thumbnail?.let {
+                builder.setLargeIcon(it)
 
-            builder.setColorized(true)
-            // scale thumbnail to a single color in two steps
-            val b1 = Bitmap.createScaledBitmap(it, 16, 16, true)
-            val b2 = Bitmap.createScaledBitmap(b1, 1, 1, true)
-            builder.setColor(b2.getPixel(0, 0))
-            b2.recycle(); b1.recycle()
+                builder.setColorized(true)
+                // scale thumbnail to a single color in two steps
+                val b1 = Bitmap.createScaledBitmap(it, 16, 16, true)
+                val b2 = Bitmap.createScaledBitmap(b1, 1, 1, true)
+                builder.setColor(b2.getPixel(0, 0))
+                b2.recycle(); b1.recycle()
+            }
         }
 
         val playPauseAction = if (paused)

commit 4567f1dadb2b4b063caa910ba9430037367c6006

BackgroundPlaybackService: fix notification thumbnail on pre-Oreo devices
Commit 5eb28a427eecd9f7f954a5242b958dc623e152a2
("BackgroundPlaybackService: skip unnecessary notification
customization attempts") restricted the manual thumbnail setup
to *only* execute on newer devices with a media token,
completely bypassing it on older devices where
manual setup is strictly required.

Fixes: 5eb28a427eecd9f7f954a5242b958dc623e152a2

app/src/main/java/is/xyz/mpv/BackgroundPlaybackService.kt
@@ -76,7 +76,7 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
 
         // With an active media session, the media style will override everything
         // (including the thumbnail) and we can skip doing this.
-        if (mediaToken != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (mediaToken == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             thumbnail?.let {
                 builder.setLargeIcon(it)
 