6fdbab9e703ba7b00e574bd188fba964201d7e6b
moi15moi <moi15moismokerlolilol@gmail.com>
2026-09-01 18:37:21 -0400

    Enable asm with libass
    This boost performance a lot.
    Note that libass doesn't support arm32 bits asm, so we won't enable it for that platform.

--- buildscripts/scripts/libass.sh
+++ buildscripts/scripts/libass.sh
@@ -13,13 +13,19 @@ fi
 
 [ -f configure ] || ./autogen.sh
 
+asm_args=
+if [ "$ndk_triple" != "arm-linux-androideabi" ]; then
+	asm_args=--enable-asm
+fi
+
 mkdir -p _build$ndk_suffix
 cd _build$ndk_suffix
 
 ../configure \
 	--host=$ndk_triple --with-pic \
 	--enable-static --disable-shared \
-	--enable-libunibreak --enable-fontconfig
+	--enable-libunibreak --enable-fontconfig \
+	$asm_args
 
 make -j$cores
 make DESTDIR="$prefix_dir" install
